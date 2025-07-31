d15fa1d32fbfb7b3d4c75213f82c7775a8421fc9
Heejung Yang <wolla39@gmail.com>
2025-07-31 14:58:51 +0900

    프로젝트 버전 및 패키지 설정 수정

--- Wellnest/Wellnest/App/WellnestApp.swift
+++ Wellnest/Wellnest/App/WellnestApp.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 @main
 struct WellnestApp: App {
     let persistenceController = PersistenceController.shared
-
-    init() {
-           FirebaseApp.configure()
-    }
 
     var body: some Scene {
         WindowGroup {
