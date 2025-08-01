4376cf412781841b96f702b145ff484d4fe02527
Heejung Yang <wolla39@gmail.com>
2025-08-01 14:57:46 +0900

    Add DailySummaryEntitu, ScheduleEntity

---
+++ Wellnest/Wellnest/Shared/Models/CoreData/ScheduleEntity/ScheduleEntity+CoreDataClass.swift
@@ -0,0 +1,12 @@
+//
+//  ScheduleEntity+CoreDataClass.swift
+//  Wellnest
+//
+//  Created by Heejung Yang on 8/1/25.
+//
+
+import Foundation
+import CoreData
+
+@objc(ScheduleEntity)
+public class ScheduleEntity: NSManagedObject {}
