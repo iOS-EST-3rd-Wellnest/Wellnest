54710550240eb8f8c1fe2afd814e8c9d8731fcd6
pde0814 <dongeon8943@gmail.com>
2025-08-01 18:12:05 +0900

    메인 탭 뷰 분기 완료

---
+++ Wellnest/Wellnest/Features/MainTab/View/MainTabView.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabView.swift
+//  Wellnest
+//
+//  Created by 박동언 on 8/1/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab: TabBarItem = .home
+    @State private var showScheduleMenu: Bool = false
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Group {
+                switch selectedTab {
+                case .home:
+                    PlanView()
+                case .plan:
+                    PlanView()
+                case .analysis:
+                    PlanView()
+                case .settings:
+                    SettingsView()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            CustomTabBar(selectedTab: $selectedTab, showScheduleMenu: $showScheduleMenu)
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
