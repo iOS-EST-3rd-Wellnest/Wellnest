636f2faa1bd6067363f57459e33d8531c882d954
전광호 <haksang1002@gmail.com>
2025-08-07 11:09:20 +0900

    UserDefault Key 추가 / 건강 앱 동기화 토글 여부 추가

--- Wellnest/Wellnest/Shared/Persistence/UserDefault/UserDefaultsKeys.swift
+++ Wellnest/Wellnest/Shared/Persistence/UserDefault/UserDefaultsKeys.swift
@@ -16,15 +16,8 @@ enum UserDefaultsKeys {
     /// 설정 화면 관련 키
     enum Settings {
         static let notificationEnabled = "notificationEnabled"
+        static let isHealthDataEnabled = "isHealthDataEnabled"
         // 설정 관련 키를 여기에 추가해주세요.
-        enum Profile {
-            static let name = "profileName"
-            static let age = "profileAge"
-            static let gender = "profileGender"
-            static let height = "profileHeight"
-            static let weight = "profileWeight"
-            static let imageData = "profileImageData"
-        }
     }
 
     // 기타 키 추가 시 다음과 같이 추가해주세요.
