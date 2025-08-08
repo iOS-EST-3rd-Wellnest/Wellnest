918bb5cc327184974a42ddc5be500adeb14321f7
전광호 <haksang1002@gmail.com>
2025-08-08 15:27:31 +0900

    NotiDelegate 코드를 AppDelegate로 이동

--- Wellnest/Wellnest/App/WellnestApp.swift
+++ Wellnest/Wellnest/App/WellnestApp.swift
@@ -9,19 +9,16 @@ import SwiftUI
 import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        LocalNotiManager.shared.localNotiDelegate()
+        return true
+    }
 }
 
 @main
 struct WellnestApp: App {
-    init() {
-        LocalNotiManager.shared.localNotiDelegate()
-    }
-    
     var body: some Scene {
         WindowGroup {
             AppRouter()
