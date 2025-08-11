81a7ac008c2f6f3b66dfd0e87f77499574732f41
전광호 <haksang1002@gmail.com>
2025-08-11 16:57:59 +0900

    설정 캘린더 뷰 UI 변경

--- Wellnest/Wellnest/Feature/Settings/SubView/CalendarInterworkView.swift
+++ Wellnest/Wellnest/Feature/Settings/SubView/CalendarInterworkView.swift
@@ -14,60 +14,96 @@ struct CalendarInterworkView: View {
     
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
+            
             Spacer()
             
-            Text("캘린더앱과 연동하여 일정을 가져옵니다.")
-                .padding()
-                .overlay {
-                    RoundedRectangle(cornerRadius: CornerRadius.medium)
-                        .stroke(lineWidth: 1)
-                }
-            
-            Toggle(isOn: $isOn) {
-                Text("캘린더 연동하기")
-            }
-            .onChange(of: isOn) { newValue in
-                if newValue {
-                    handleCalendarAccess()
+            VStack(alignment: .leading, spacing: 40) {
+                HStack {
+                    Image(systemName: "calendar")
+                        .foregroundStyle(.blue)
+                        .font(.system(size: 40))
                     
-                    let store = EKEventStore()
-                    store.requestAccess(to: .event) { granted, error in
-                        if let error {
-                            print("에러발생: \(error)")
-                        }
+                    VStack(alignment: .leading) {
+                        Text("당신의 하루를 한눈에")
+                            .font(.title3)
+                            .fontWeight(.bold)
                         
-                        if granted {
-                            print("캘린더 접근 허용")
-                        } else {
-                            print("캘린더 접근 거부")
-                        }
+                        Text("캘란더를 연동하면 일정과 목표를 함께 관리할 수 있습니다.")
+                            .font(.footnote)
+                    }
+                }
+                
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundStyle(.blue)
+                        .font(.system(size: 40))
+                    
+                    VStack(alignment: .leading) {
+                        Text("계획부터 실행까지")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        
+                        Text("캘린더를 연동하여 오늘 할 일과 건강 목표를 함께 체크하세요.")
+                            .font(.footnote)
                     }
                 }
             }
             
             Spacer()
             
-            if isOn {
-                // TODO: 연동코드 작성
+            Button {
+                
+            } label: {
+                Text("캘린더 앱 연동하기")
+                    .padding(Spacing.content)
+                    .frame(maxWidth: .infinity)
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .padding(.bottom, 100)
         }
+//        VStack {
+//            Spacer()
+//            
+//            Text("캘린더앱과 연동하여 일정을 가져옵니다.")
+//                .padding()
+//                .overlay {
+//                    RoundedRectangle(cornerRadius: CornerRadius.medium)
+//                        .stroke(lineWidth: 1)
+//                }
+//            
+//            Toggle(isOn: $isOn) {
+//                Text("캘린더 연동하기")
+//            }
+//            .onChange(of: isOn) { newValue in
+//                if newValue {
+//                    handleCalendarAccess()
+//                    
+//                    let store = EKEventStore()
+//                    store.requestAccess(to: .event) { granted, error in
+//                        if let error {
+//                            print("에러발생: \(error)")
+//                        }
+//                        
+//                        if granted {
+//                            print("캘린더 접근 허용")
+//                        } else {
+//                            print("캘린더 접근 거부")
+//                        }
+//                    }
+//                }
+//            }
+//            
+//            Spacer()
+//            
+//            if isOn {
+//                // TODO: 연동코드 작성
+//            }
+//        }
         .padding()
         .navigationTitle("캘린더 연동")
         .navigationBarTitleDisplayMode(.inline)
-        .alert("캘린더 접근이 필요합니다.", isPresented: $showSettingAlert) {
-            Button("설정으로 이동") {
-                if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
-                    UIApplication.shared.open(url)
-                }
-            }
-            
-            Button("취소", role: .cancel) {
-                isOn = false
-            }
-        } message: {
-            Text("설정에서 캘린더 앱의 접근을 허용해주세요.")
-        }
     }
     
     func handleCalendarAccess() {
