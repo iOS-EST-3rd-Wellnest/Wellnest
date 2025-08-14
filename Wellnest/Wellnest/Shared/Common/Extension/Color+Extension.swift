071511b539955efbfb95669267431973ef7ee0a8
pde0814 <dongeon8943@gmail.com>
2025-08-14 10:48:11 +0900

    컬러 임시로 설정

---
+++ Wellnest/Wellnest/Shared/Common/Extension/Color+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  Color+Extension.swift
+//  Wellnest
+//
+//  Created by 박동언 on 8/13/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static func scheduleBackground(color: String) -> Color {
+        switch color.lowercased() {
+        case "red": return .red.opacity(0.3)
+        case "blue": return .blue.opacity(0.3)
+        case "green": return .green.opacity(0.3)
+        case "yellow": return .yellow.opacity(0.3)
+        case "orange": return .orange.opacity(0.3)
+        case "purple": return .purple.opacity(0.3)
+        case "pink": return .pink.opacity(0.3)
+        case "gray": return .gray.opacity(0.3)
+        default: return .gray.opacity(0.3)
+        }
+    }
+
+    static func scheduleSolid(color: String) -> Color {
+        switch color.lowercased() {
+        case "red": return .red
+        case "blue": return .blue
+        case "green": return .green
+        case "yellow": return .yellow
+        case "orange": return .orange
+        case "purple": return .purple
+        case "pink": return .pink
+        case "gray": return .gray
+        default: return .blue
+        }
+    }
+
+    static func scheduleDot(color: String) -> Color {
+        return scheduleSolid(color: color)
+    }
+}
