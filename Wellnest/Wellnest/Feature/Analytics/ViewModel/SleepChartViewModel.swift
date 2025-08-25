69ce6aff9f58c81908f5afcef5d5e5603b9f546b
이준일 <junil2536@naver.com>
2025-08-25 11:20:44 +0900

    analytics view data 수정

--- Wellnest/Wellnest/Feature/Analytics/ViewModel/SleepChartViewModel.swift
+++ Wellnest/Wellnest/Feature/Analytics/ViewModel/SleepChartViewModel.swift
@@ -9,13 +9,13 @@ import Foundation
 
 class SleepChartViewModel: ObservableObject {
     @Published var selectedPeriod: ChartPeriod = .week
-    
+
     private let sleepData: SleepData
-    
+
     init(sleepData: SleepData) {
         self.sleepData = sleepData
     }
-    
+
     var currentChartData: [Double] {
         switch selectedPeriod {
         case .week:
@@ -24,15 +24,15 @@ class SleepChartViewModel: ObservableObject {
             return sleepData.monthlySleepHours
         }
     }
-    
+
     var averageSleepTime: String {
         "\(Int(sleepData.averageHours))시간 \(sleepData.averageMinutes)분"
     }
-    
+
     var sleepQuality: String {
         "\(sleepData.sleepQuality)%"
     }
-    
+
     var qualityChangeText: String {
         "+\(sleepData.qualityChange)%"
     }
