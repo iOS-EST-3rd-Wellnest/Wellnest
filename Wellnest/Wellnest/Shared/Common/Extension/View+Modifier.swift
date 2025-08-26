379426525eac1c6f16a3d75647026b41c25d6d42
pde0814 <dongeon8943@gmail.com>
2025-08-26 11:08:36 +0900

    그림자, 테두리 좀더 연하게 처리

--- Wellnest/Wellnest/Shared/Common/Extension/View+Modifier.swift
+++ Wellnest/Wellnest/Shared/Common/Extension/View+Modifier.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct RoundedBorderModifier: ViewModifier {
     var cornerRadius: CGFloat
-    var color: Color = .secondary.opacity(0.2)
-    var lineWidth: CGFloat = 0.5
+    var color: Color
+    var lineWidth: CGFloat
 
     func body(content: Content) -> some View {
         content
@@ -24,12 +24,12 @@ struct RoundedBorderModifier: ViewModifier {
 
 extension View {
     func defaultShadow() -> some View {
-        self.shadow(color: .secondary.opacity(0.2), radius: 8, x: 6, y: 6)
+        self.shadow(color: .black.opacity(0.1), radius: 8, x: 6, y: 6)
     }
 
     func roundedBorder(
         cornerRadius: CGFloat,
-        color: Color = .secondary.opacity(0.6),
+        color: Color = .secondary.opacity(0.5),
         lineWidth: CGFloat = 0.2
     ) -> some View {
         self.modifier(RoundedBorderModifier(
