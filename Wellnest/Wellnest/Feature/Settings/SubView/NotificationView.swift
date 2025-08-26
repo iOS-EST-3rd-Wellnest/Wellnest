46d8071566a51263114bcd1e8497dc886901ec05
전광호 <haksang1002@gmail.com>
2025-08-26 13:41:43 +0900

    설정 UI 수정

--- Wellnest/Wellnest/Feature/Settings/SubView/NotificationView.swift
+++ Wellnest/Wellnest/Feature/Settings/SubView/NotificationView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import UserNotifications
 
 struct NotificationView: View {
+    @State private var showAlert: Bool = false
     @StateObject private var userDefault = UserDefaultsManager.shared
     
     var body: some View {
@@ -17,8 +18,25 @@ struct NotificationView: View {
         }
         .onChange(of: userDefault.isNotificationEnabled) { newValue in
             if newValue {
-                LocalNotiManager.shared.requestNotificationAuthorization()
+                LocalNotiManager.shared.requestNotificationAuthorization { granted in
+                    if !granted {
+                        userDefault.isNotificationEnabled = false
+                        showAlert = true
+                    }
+                }
             }
+        }
+        .tint(.wellnestOrange)
+        .alert("알림 권한이 꺼져있습니다.", isPresented: $showAlert) {
+            Button("설정으로 이동") {
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+            
+            Button("취소", role: .cancel) { }
+        } message: {
+            Text("설정 > Wellnest 알림 권한을 허용해야 알림을 받을 수 있습니다.")
         }
     }
 }
